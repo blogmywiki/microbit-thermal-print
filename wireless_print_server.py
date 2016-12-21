5c42734e20a41f421f5022a96400d1706c8adb87
Giles Booth <giles.booth@gmail.com>
2016-12-21 11:12:33 +0000

    Create wireless_print_server.py

---
+++ wireless_print_server.py
@@ -0,0 +1,12 @@
+# this will print any radio messages received
+
+from microbit import *
+import radio
+
+uart.init(baudrate=19200, bits=8, parity=None, stop=1, tx=pin8, rx=None)
+
+radio.on()
+while True:
+    incoming = radio.receive()
+    if incoming:
+        uart.write(incoming+"\n\n\n")
